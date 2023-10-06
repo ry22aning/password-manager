640d8727b5da5b842e4f98e17a4f252e7d5115d2
Ryaan <141950067+ryaansec@users.noreply.github.com>
2023-10-06 23:30:47 +0530

    psdw.c

--- password manager/psdw.c
+++ password manager/psdw.c
@@ -1,39 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <unistd.h> // For usleep function in loading
+#include <stdbool.h> // For boolean type
+#include <ctype.h>
+
+int isStrongPassword(const char *password) {
+    int length = strlen(password);
+    int hasDigit = 0;
+    int hasLower = 0;
+    int hasUpper = 0;
+    int hasSpecial = 0;
+
+    for (int i = 0; i < length; i++) {
+        if (isdigit(password[i])) {
+            hasDigit = 1;
+        } else if (islower(password[i])) {
+            hasLower = 1;
+        } else if (isupper(password[i])) {
+            hasUpper = 1;
+        } else if (ispunct(password[i])) {
+            hasSpecial = 1;
+        }
+    }
+
+    // Check password strength based on criteria
+    if (length >= 8 && hasDigit && hasLower && hasUpper && hasSpecial) {
+        return 1; // Strong password
+    } else {
+        return 0; // Weak password
+    }
+}
 
 struct PasswordEntry {
+    char email[50];
     char website[50];
     char username[50];
     char password[50];
-    char email[50];
 };
+
+
+
 
 void addPassword(struct PasswordEntry **passwords, int *count) {
     *passwords = realloc(*passwords, (*count + 1) * sizeof(struct PasswordEntry));
 
-    printf("Enter website: ");
-    fgets((*passwords)[*count].website, sizeof((*passwords)[*count].website), stdin);
+    char password[50];
 
-    printf("Enter the e-mail address you've registered with: ");
-    fgets((*passwords)[*count].email, sizeof((*passwords)[*count].email), stdin);
+
+    printf("Enter the E-mail you've regestered with: ");
+    scanf("%s", (*passwords)[*count].email);
+
+    printf("Enter website: ");
+    scanf("%s", (*passwords)[*count].website);
 
     printf("Enter username: ");
-    fgets((*passwords)[*count].username, sizeof((*passwords)[*count].username), stdin);
+    scanf("%s", (*passwords)[*count].username);
 
     printf("Enter password: ");
-    fgets((*passwords)[*count].password, sizeof((*passwords)[*count].password), stdin);
+    scanf("%s", (*passwords)[*count].password);
+
+    if (isStrongPassword(password)) {
+        printf("Password is strong.\n");
+    } else {
+        printf("Hey dear terminal user, how could you password be still weak if you're running this 80's looking program.\n");
+    }
+
 
     (*count)++;
-
 }
 
 void displayPasswords(struct PasswordEntry *passwords, int count) {
     printf("Stored Passwords:\n");
     for (int i = 0; i < count; i++) {
+        printf("Website: %s\n", passwords[i].email);
         printf("Website: %s\n", passwords[i].website);
-        printf("Email: %s\n", passwords[i].email);
         printf("Username: %s\n", passwords[i].username);
         printf("Password: %s\n", passwords[i].password);
         printf("\n");
@@ -50,7 +91,7 @@ void savePasswordsToFile(struct PasswordEntry *passwords, int count) {
     }
 
     for (int i = 0; i < count; i++) {
-        fprintf(file, "%s %s %s %s\n", passwords[i].website, passwords[i].email, passwords[i].username, passwords[i].password);
+        fprintf(file, "%s %s %s %s\n",passwords[i].email, passwords[i].website, passwords[i].username, passwords[i].password);
     }
 
     fclose(file);
@@ -64,7 +105,7 @@ void loadPasswordsFromFile(struct PasswordEntry **passwords, int *count) {
     }
 
     struct PasswordEntry entry;
-    while (fscanf(file, "%s %s %s %s", entry.website, entry.email, entry.username, entry.password) != EOF) {
+    while (fscanf(file, "%s %s  %s %s", entry.email, entry.website, entry.username, entry.password) != EOF) {
         *passwords = realloc(*passwords, (*count + 1) * sizeof(struct PasswordEntry));
         (*passwords)[*count] = entry;
         (*count)++;
@@ -81,22 +122,22 @@ int main() {
     int choice;
         loadPasswordsFromFile(&passwords, &count);
 
-
         const char *command22 = "figlet Pswd Manager";
         int figletwelcome = system(command22);
 
         printf("", figletwelcome);
         //printf("\nWelcome to Password Manager\n\n");
-        printf("------MENU------");
-        printf("\n1. Add Password\n");
-        printf("2. Display Passwords\n");
-        printf("3. Back up your existing passwords\n");
-        printf("4. Exit\n");
-        printf("-------------");
-        printf("\nEnter your choice: ");
-        scanf("%d", &choice);
+        while (1) {
+            printf("------MENU------\n");
+            printf("1. Add Password\n");
+            printf("2. Display Passwords\n");
+            printf("3. Back up your existing passwords\n");
+            printf("4. Exit\n");
+            printf("-------------\n");
+            printf("Enter your choice: ");
+            scanf("%d", &choice);
 
-        if (choice == 1) {
+             if (choice == 1) {
 
             //using bash commands
             const char *command = "clear";
@@ -112,44 +153,39 @@ int main() {
 
         }
             else if (choice == 3) {
+            //printf("Backing up your data into your root folder......\n");
+           // const char *command3 = "sudo cp ~/Desktop/passwords.txt /root/rootPswd.txt ";   
+            const char *command3 = "sudo cp ~/Downloads/password manager/passwords.txt /root/rootPswd.txt";
+             int backupRoot = system(command3);
+            printf("\n", backupRoot);
 
-    const char *mkdirCommand = "sudo mkdir -p /root/";
-    int mkdirResult = system(mkdirCommand);
-    
-    if (mkdirResult != 0) {
-        printf("Error creating the destination directory.\n");
-        return 1; // You can choose an appropriate error code here
-    }
-
-    const char *command3 = "sudo cp passwords.txt /root/rootPswd.txt -v";
-    int backupRoot = system(command3);
-    printf("\n", backupRoot);
-
-    for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) { //For the loading function of backup 
         printf("\rBacking up...... %d%%", i * 10);
-        fflush(stdout);
-        usleep(500000);
+        fflush(stdout); // Flush the output to make sure it's immediately visible
+        usleep(500000); // Sleep for 0.5 seconds
     }
     printf("\nBackup complete.\n");
 
 
 
-
-
+        } 
         
-
-
-
-        } else if (choice == 4) {
+        else if (choice == 4) {
             printf("Exiting...\n");
             free(passwords);
             exit(0);
 
 
+        }
 
-        } else {
+        
+        else {
             printf("Invalid input\n");
         }
     }
+}
+
+    
+
 
 
